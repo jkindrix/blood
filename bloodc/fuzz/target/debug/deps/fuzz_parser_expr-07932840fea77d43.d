448ed2a0a1ed7d525cedf3ed9fb300ca37dec851
Justin Kindrix <jkindrix@gmail.com>
2026-01-09 10:34:31 +0000

    chore: add .gitignore and remove build artifacts
    Add .gitignore for:
    - Build artifacts (target/)
    - IDE files
    - OS files
    - Temporary files
    - Fuzzing artifacts
    
    Remove accidentally committed fuzz/target/ directory.

--- bloodc/fuzz/target/debug/deps/fuzz_parser_expr-07932840fea77d43.d
+++
@@ -1,5 +0,0 @@
-/home/jkindrix/blood/bloodc/fuzz/target/debug/deps/fuzz_parser_expr-07932840fea77d43.d: fuzz_targets/fuzz_parser_expr.rs
-
-/home/jkindrix/blood/bloodc/fuzz/target/debug/deps/libfuzz_parser_expr-07932840fea77d43.rmeta: fuzz_targets/fuzz_parser_expr.rs
-
-fuzz_targets/fuzz_parser_expr.rs:
